3571ea00d223abf0943fbc39eee0cdbaa51fd007
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-11-21 15:38:19 -0500

    cleanup syntax

--- parametrize_c_in.py
+++ parametrize_c_in.py
@@ -1,20 +1,29 @@
-from dolfinx.log import set_log_level, LogLevel
 from two_volume_festim_model import build_festim_model
-
 
 N_A = 6.022e23
 
-# mol_m3_c_in = [5e-9,5e-8,5e-7,5e-6,5e-5,5e-4,5e-3,5e-2,5e-1,5,5e1]  # mol/m3, corresponds to about 3e15 to 3e25 atms/m3
-mol_m3_c_in = [6.5e-5, 1e2]  # mol/m3, corresponds to 1e19 and 6e25 #/m3
+mol_m3_c_in = [
+    5e-9,
+    5e-8,
+    5e-7,
+    5e-6,
+    5e-5,
+    5e-4,
+    5e-3,
+    5e-2,
+    5e-1,
+    5,
+    5e1,
+]  # mol/m3, corresponds to about 3e15 to 3e25 atms/m3
 
 for conc in mol_m3_c_in:
     c_in = conc * N_A  # atms/m3
 
-    print(f"Running simulation for inlet Concentration of {c_in} #/m3.")
+    print(f"Running simulation for inlet concentration of {c_in} #/m3.")
 
     my_model = build_festim_model(
-        openfoam_data_file="OpenFOAM/laminar-case/probe.foam",
-        openfoam_final_time=300,
+        openfoam_data_file="OpenFOAM/kOmega-case/case.foam",
+        openfoam_final_time=208,
         breeder_temperature=603.15,
         delta=0.1,
         c_in=c_in,
@@ -25,5 +34,6 @@ for conc in mol_m3_c_in:
 
     # INITIALISE AND RUN
     my_model.initialise()
-    set_log_level(LogLevel.INFO)
     my_model.run()
+
+    print(f"Simulation for inlet concentration {c_in} #/m3 complete.")

da5fd3da4328085a47f7485d60221aee105f37e9
kaelyndunnell <kaelyndunnell03@gmail.com>
2026-01-24 18:50:36 +0530

    add two more concs

--- parametrize_c_in.py
+++ parametrize_c_in.py
@@ -1,12 +1,14 @@
-from two_volume_festim_probe_out import build_festim_model
+# from two_volume_festim_probe_out import build_festim_model
 
-# from two_volume_festim_probe_in import build_festim_model
+from two_volume_festim_probe_in import build_festim_model
 
 N_A = 6.022e23
 
 mol_m3_c_in = [
     5e-5,
     1e-4,
+    5e-4,
+    1e-3,
     5e-3,
     1e-2,
     1e-1,
@@ -22,33 +24,33 @@ for conc in mol_m3_c_in:
 
     print(f"Running simulation for inlet concentration of {c_in} #/m3.")
 
-    # # PROBE IN MODEL
-    # my_model = build_festim_model(
-    #     openfoam_data_file="OpenFOAM/benchmark_cases_LiPb/kOmega-case/case.foam",
-    #     openfoam_final_time=208,
-    #     festim_mesh_file="meshing/probe_in_festim_mesh.msh",
-    #     breeder_temperature=603.15,
-    #     delta=10,
-    #     c_in=c_in,
-    #     Sc=0.7,
-    #     results_folder=f"surfaceRecomboBC/festim_abacus_probe_in/festim_results_cin_{conc}_mol_m3",
-    #     insulated=True,
-    #     visualize_fields=False,
-    # )
-
-    # PROBE OUT MODEL
+    # PROBE IN MODEL
     my_model = build_festim_model(
-        openfoam_data_file="OpenFOAM/benchmark_cases_LiPb/probe_out_kOmegaSST/case.foam",
-        openfoam_final_time=2457,
-        festim_mesh_file="meshing/probe_out_festim_mesh.msh",
+        openfoam_data_file="OpenFOAM/benchmark_cases_LiPb/kOmega-case/case.foam",
+        openfoam_final_time=208,
+        festim_mesh_file="meshing/probe_in_festim_mesh.msh",
         breeder_temperature=603.15,
-        delta=0.1,
+        delta=10,
         c_in=c_in,
-        Sc=0.7,  # seems to be default in OpenFOAM, find a reference to back up
-        results_folder=f"surfaceRecomboBC/festim_abacus_probe_out/festim_results_cin_{conc}_mol_m3",
+        Sc=0.7,
+        results_folder=f"surfaceRecomboBC/festim_abacus_probe_in/festim_results_cin_{conc}_mol_m3",
         insulated=True,
         visualize_fields=False,
     )
+
+    # PROBE OUT MODEL
+    # my_model = build_festim_model(
+    #     openfoam_data_file="OpenFOAM/benchmark_cases_LiPb/probe_out_kOmegaSST/case.foam",
+    #     openfoam_final_time=2457,
+    #     festim_mesh_file="meshing/probe_out_festim_mesh.msh",
+    #     breeder_temperature=603.15,
+    #     delta=0.1,
+    #     c_in=c_in,
+    #     Sc=0.7,  # seems to be default in OpenFOAM, find a reference to back up
+    #     results_folder=f"surfaceRecomboBC/festim_abacus_probe_out/festim_results_cin_{conc}_mol_m3",
+    #     insulated=True,
+    #     visualize_fields=False,
+    # )
 
     # INITIALISE AND RUN
     my_model.initialise()
